e06ec2293911b01bc78025f68f0a650604406134
Luís Henrique de Azevedo Melo <lhazmelo@gmail.com>
2026-05-11 16:53:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ import pandas as pd
 from sqlalchemy import create_engine
 from shapely.geometry import Point, Polygon
 from scipy.interpolate import Rbf
+import folium
+from streamlit_folium import st_folium
+from folium.plugins import HeatMap
 
 st.title("🌡️ Mapa de Calor Geociências - Nuvem")
 
 # 1. CONECTA NO SUPABASE
 @st.cache_data(ttl=10) # Atualiza a cada 10 segundos
 def puxar_dados():
-    # Ele vai ler a senha escondida que configuraremos no Streamlit
+   
     engine = create_engine(st.secrets["DB_URL"])
     query = "SELECT sensor_id, latitude, longitude, temperatura, data_hora FROM medicoes ORDER BY data_hora DESC LIMIT 4"
     return pd.read_sql_query(query, engine)
@@ -39,5 +42,40 @@ if poligono_geo.contains(ponto_usuario):
 else:
     st.error("Fora da área de cobertura.")
 
-st.write("Dados no Banco de Dados (Supabase):")
-st.dataframe(df_sensores)
+
+# 5. DESENHO DO MAPA VISUAL
+st.subheader("🗺️ Visualização do Prédio de Geociências")
+
+#CENTRALIZAÇÃO DO MAPA
+mapa = folium.Map(location=[-22.7694, -43.6875], zoom_start=18)
+
+dados_heatmap = []
+
+#MARCADOR VERMELHO PARA CADA SENSOR
+for index, linha in df_sensores.iterrows():
+    lat = linha['latitude']
+    lon = linha['longitude']
+    temp = linha['temperatura']
+    
+    folium.Marker(
+        location=[lat, lon],
+        popup=f"Sensor {linha['sensor_id']}: {temp}°C",
+        icon=folium.Icon(color="red", icon="fire"),
+    ).add_to(mapa)
+    
+    #CRIA UM PESO TÉRMICO
+    dados_heatmap.append([lat, lon, temp])
+
+#CAMADA DE CORES
+HeatMap(dados_heatmap, radius=40, blur=25, max_zoom=1).add_to(mapa)
+
+#MOSTRA ONDE O ALUNO ESTÁ NO MAPA
+if poligono_geo.contains(ponto_usuario):
+    folium.Marker(
+        location=[lat_usuario, lon_usuario],
+        popup="Você está aqui",
+        icon=folium.Icon(color="blue", icon="user"),
+    ).add_to(mapa)
+
+#JOGA O MAPA NA TELA
+st_folium(mapa, width=700, height=500)
